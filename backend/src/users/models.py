1e0406decc99e6a6863789e33d3c1710f4d68207
Ivan Kupreev <ivan.kupreev03@gmail.com>
2026-09-19 15:10:50 +0400

    add core/database.py and users/models.py with alembic

---
+++ backend/src/users/models.py
@@ -0,0 +1,23 @@
+import enum
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.core.database import Base, TimestampMixin, intpk, str_100
+
+
+class UserRole(enum.Enum):
+    parent = "parent"
+    tutor = "tutor"
+    admin = "admin"
+
+
+class UserORM(TimestampMixin, Base):
+    __tablename__ = "users"
+
+    id: Mapped[intpk]
+    email: Mapped[str_100]
+    hashed_password: Mapped[str]
+    first_name: Mapped[str_100]
+    last_name: Mapped[str_100]
+    middle_name: Mapped[str_100 | None]
+    role: Mapped[UserRole]
