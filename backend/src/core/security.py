cbc7ec583068de53471b7d55cda0423a4c6752a6
Ivan Kupreev <ivan.kupreev03@gmail.com>
2026-09-26 22:15:22 +0400

    Add hashing password

---
+++ backend/src/core/security.py
@@ -0,0 +1,13 @@
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
+
+
+def get_password_hash(password: str) -> str:
+    """Хэширует пароль."""
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Проверяет, соответствует ли пароль хэшу."""
+    return password_hash.verify(plain_password, hashed_password)
--- backend/src/users/service.py
+++ backend/src/users/service.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException
+from fastapi.concurrency import run_in_threadpool
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.core.security import get_password_hash
 from src.users.models import UserORM
 from src.users.schemas import UserAddDTO, UserCreateInDB
 
@@ -11,11 +13,12 @@ async def _get_user_by_email(email: str, db: AsyncSession):
 
 
 async def create_user_service(user_data: UserAddDTO, db: AsyncSession):
+    hashed_password = await run_in_threadpool(get_password_hash, user_data.password)
     if await _get_user_by_email(user_data.email, db):
         raise HTTPException(status_code=400, detail="Email already registered")
     user_in_db = UserCreateInDB(
         email=user_data.email,
-        hashed_password=user_data.password,
+        hashed_password=hashed_password,
         first_name=user_data.first_name,
         last_name=user_data.last_name,
         middle_name=user_data.middle_name,
